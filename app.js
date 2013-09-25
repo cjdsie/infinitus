623aac5547c8f86e7df551ffcdbd3a17a824e293
cjdsie <cjdsie@gmail.com>
2013-09-25 17:23:33 -0230

    menu size up

--- app.js
+++ app.js
@@ -25,6 +25,80 @@ app.use(app.router);
 app.use(require('stylus').middleware(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+var pkgcloud = require('pkgcloud'),
+    _ = require('underscore');
+
+// create our client with your rackspace credentials
+var client = pkgcloud.providers.rackspace.compute.createClient({
+  username: 'chrisdasie',
+  apiKey:   'your-api-key'
+});
+
+// first we're going to get our flavors
+client.getFlavors(function (err, flavors) {
+    if (err) {
+        console.dir(err);
+        return;
+    }
+
+    // then get our base images
+    client.getImages(function (err, images) {
+        if (err) {
+            console.dir(err);
+            return;
+        }
+
+        // Pick a 512MB instance flavor
+        var flavor = _.findWhere(flavors, { name: '512MB Standard Instance' });
+
+        // Pick an image based on Ubuntu 12.04
+        var image = _.findWhere(images, { name: 'Ubuntu 12.04 LTS (Precise Pangolin)' });
+
+        // Create our first server
+        client.createServer({
+            name: 'server1',
+            image: image,
+            flavor: flavor
+        }, handleServerResponse);
+
+        // Create our second server
+        client.createServer({
+            name: 'server2',
+            image: image,
+            flavor: flavor
+        }, handleServerResponse);
+    });
+});
+
+// This function will handle our server creation,
+// as well as waiting for the server to come online after we've
+// created it.
+function handleServerResponse(err, server) {
+    if (err) {
+        console.dir(err);
+        return;
+    }
+
+    console.log('SERVER CREATED: ' + server.name + ', waiting for active status');
+
+    // Wait for status: ACTIVE on our server, and then callback
+    server.setWait({ status: 'ACTIVE' }, 5000, function (err) {
+        if (err) {
+            console.dir(err);
+            return;
+        }
+
+        console.log('SERVER INFO');
+        console.log(server.name);
+        console.log(server.status);
+        console.log(server.id);
+
+        console.log('Make sure you DELETE server: ' + server.id +
+            ' in order to not accrue billing charges');
+    });
+}
+
 var Poet = require('poet');
 
 var poet = Poet(app, {
